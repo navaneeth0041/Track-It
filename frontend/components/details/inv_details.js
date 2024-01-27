ae143f4cde7b88447b6856c15651bb30e1e8d55d
Chinmay <chinmayajith30@gmail.com>
2024-01-27 14:03:57 +0530

    added add/deduct stock api reqs

--- frontend/components/details/inv_details.js
+++ frontend/components/details/inv_details.js
@@ -1,7 +1,108 @@
 
+import { Fragment } from "react";
 import { FaUpload,FaDownload } from "react-icons/fa";
-const inv_details= ({itemData})=>{
 
+import React, {useState , useEffect} from "react";
+import Popup from 'reactjs-popup';
+
+const inv_details= ({itemData})=>{
+  const [value, setValue] = useState('');
+  const handleInputChange = (e) => {
+    const numericValue = e.target.value.replace(/[^0-9]/g,'');
+    setValue(numericValue);
+}
+const [itemDatas , setItemDatas] = useState(itemData)
+    useEffect(()=> {
+      fetch('http://127.0.0.1:8000/api/get-items', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+       setItemDatas(data)
+      })
+      .catch((error) => {
+       console.error('Error:', error);
+       return 'An error occurred'
+      });
+    },  [])
+
+const handleAddSubmit = (id , qty) => {
+  const requestData = {
+    "product_id": id,
+    "quantity": parseInt(qty),
+  };
+
+
+  fetch('http://127.0.0.1:8000/api/add-stock/', {  
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(requestData)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      fetch('http://127.0.0.1:8000/api/get-items', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+         setItemDatas(data)
+        })
+        .catch((error) => {
+         console.error('Error:', error);
+         return 'An error occurred'
+        });
+      // window.location.reload();
+    })
+    .catch((error) => {
+     console.error('Error:', error);
+     return 'An error occurred'
+    });
+}
+const handleDeductSubmit = (id , qty) => {
+  const requestData = {
+    "product_id": id,
+    "quantity": parseInt(qty),
+  };
+
+
+  fetch('http://127.0.0.1:8000/api/deduct-stock/', {  
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body:JSON.stringify(requestData)
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      fetch('http://127.0.0.1:8000/api/get-items', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+         setItemDatas(data)
+        })
+        .catch((error) => {
+         console.error('Error:', error);
+         return 'An error occurred'
+        });
+      // window.location.reload();
+    })
+    .catch((error) => {
+     console.error('Error:', error);
+     return 'An error occurred'
+    });
+}
     return(
         <div>
             <div className="w-full h-full bg-white px-4 rounded-lg">
@@ -19,24 +120,68 @@ const inv_details= ({itemData})=>{
             </thead>
 
             <tbody>
-              {itemData.map((item)=> (
+              {itemDatas.map((item)=> (
                 <tr key={item.product_id} className="flex justify-between border-b border-gray-200 py-2 w-full">
                   <td className="w-[30%] flex justify-start">{item.item_name}</td>
                   <td className="w-[30%] flex justify-start">{item.product_id}</td>
                   <td className="w-[30%] flex justify-start">{item.group}</td>
                   <td className="w-[30%] flex justify-start">{item.quantity}</td>
                   <td className="w-[30%] flex justify-start">
-                  <div>
-                    <button className="bg-blue-200 text-blue-800 px-5 py-2 rounded-lg text-sm">
-                      <FaUpload />
-                    </button>
+                  <div >
+                    <Fragment>
+                    <Popup trigger=
+                        {<button className="bg-blue-200 text-white px-5 py-2 rounded-md text-sm"> <FaUpload className="text-blue-900"/> </button>} 
+                        modal nested>
+                        {
+                            close => (
+                                <div class='modal'className=" w-full text-black bg-gray-300 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" >
+                                    <div className='content' >
+                                        Add Stock to item {item.product_name}
+                                    </div>
+                                    <input 
+                                      type="text" value={value} onChange={handleInputChange} 
+                                      class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                                      <div>
+                                        <button className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center" 
+                                        onClick=
+                                            {() => {handleAddSubmit(item.product_id , value); close(); setValue('')}}>
+                                                Submit
+                                        </button>
+                                    </div>
+                                </div>
+                            )
+                        }
+                    </Popup>
+                    </Fragment>
                   </div> 
                   </td>
                   <td className="w-[30%] flex justify-start]">
                   <div>
-                    <button className="bg-blue-200 text-white px-5 py-2 rounded-md text-sm">
-                      <FaDownload className="text-red-500" />
-                    </button>
+                    <Fragment>
+                    <Popup trigger=
+                          {<button className="bg-blue-200 text-white px-5 py-2 rounded-md text-sm"> <FaDownload className="text-red-500"/> </button>} 
+                          modal nested>
+                          {
+                              close => (
+                                  <div class='modal' className="w-full text-black bg-gray-300 hover:bg-gray-300 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                                      <div class='content' >
+                                          Deduct Stock to item {item.product_id}
+                                      </div>
+                                      <input 
+                                        type="text" value={value} onChange={handleInputChange} 
+                                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
+                                        <div>
+                                          <button className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                                          onClick=
+                                              {() => {handleDeductSubmit(item.product_id , value);close(); setValue('')}}>
+                                                  Submit
+                                          </button>
+                                      </div>
+                                  </div>
+                              )
+                          }
+                      </Popup>
+                      </Fragment>
                   </div>
                 </td>
                 </tr>
@@ -51,3 +196,20 @@ const inv_details= ({itemData})=>{
 
 }
 export default inv_details;
+
+
+{/* <Modal isVisible={showModaldown} onClose={()=> setShowModaldown(false)}>
+                      <div className="p-6">
+                        <h3 className="text-xl font-semibold text-gray-900 mb-5">
+                          Modal Title
+                          </h3>
+                          <form class='space-y-6'action="#">
+                              <div>
+                                <label className="block mb-2 text-sm font-medium text-gray-900">
+                                  Enter stock subtracted
+                                </label>
+                                </div>
+                              
+                          </form>
+                      </div>
+                      </Modal> */}
